78194335c1e04bab9209d6ba56084d67073bb950
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-04-20 17:10:32 -0400

    stream

--- src/Chloe.Api.Tests/Controllers/TestEmployeeController.cs
+++ src/Chloe.Api.Tests/Controllers/TestEmployeeController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Http;
 using Unity.WebApi;
+using System.IO;
 
 namespace Chloe.Api.Tests.Controllers
 {
@@ -37,7 +38,27 @@ namespace Chloe.Api.Tests.Controllers
         [TestMethod]
         public async Task GetMethod()
         {
+            //http://stackoverflow.com/questions/24779749/c-sharp-read-from-httpresponsemessage
+
             HttpResponseMessage response = await testServer.CreateRequest("/api/employee/get").GetAsync();
+
+            if (response != null)
+            {
+                var responseValue = string.Empty;
+
+                Task task = response.Content.ReadAsStreamAsync().ContinueWith(t =>
+                {
+                    var stream = t.Result;
+                    using (var reader = new StreamReader(stream))
+                    {
+                        responseValue = reader.ReadToEnd();
+                    }
+                });
+
+                task.Wait();
+                
+            }
+
             var result = await response.Content.ReadAsAsync<ICollection<EmployeeDto>>();
             Assert.AreEqual(result.Count, 1);
         }
